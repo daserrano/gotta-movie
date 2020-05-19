5f68ee4de236576ccf5eccfa41185a098d7ad8fa
daserrano <sein2809@gmail.com>
2020-05-20 01:31:58 +0200

    Formulario buscar pelicula

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import './App.css';
 import 'bulma/css/bulma.css'
 import {Title} from './components/Title'
+import {SearchForm} from './components/SearchForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Title>Search Movies</Title>
+        <div className='SearchForm-wrapper'>
+          <SearchForm/>
+        </div>
       </div>
     );
   }
